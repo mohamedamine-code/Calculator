bc954dd45d9d50e4626b108e47ac829ffde3297a
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-02-26 15:29:43 +0100

    Color

--- lib/Pagehome.dart
+++ lib/Pagehome.dart
@@ -8,27 +8,41 @@ class Pagehome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<String> l1 = [
-      '(',
-      ')',
-      '%',
       'AC',
+      '+/-',
+      '%',
+      '/',
       '7',
       '8',
       '9',
-      '/',
+      'x',
       '4',
       '5',
       '6',
-      'x',
+      '-',
       '1',
       '2',
       '3',
-      '-',
-      '0',
-      '.',
-      '=',
       '+',
+      '',
+      '0',
+      ',',
+      '=',
     ];
+
+    bool isOperator(String ch) {
+      if (ch == "(" ||
+          ch == ")" ||
+          ch == "=" ||
+          ch == "/" ||
+          ch == "x" ||
+          ch == "-" ||
+          ch == "+") {
+        return true;
+      }
+      return false;
+    }
+
     return MaterialApp(
         home: Scaffold(
             body: Column(
@@ -36,34 +50,42 @@ class Pagehome extends StatelessWidget {
         Expanded(
             flex: 1,
             child: Container(
-              color: Colors.grey,
+              color: Colors.black,
             )),
-        Expanded(
-          flex: 2,
-          child: GridView.builder(
-            padding: EdgeInsets.only(
-              top: 30,
+        Container(
+          color: Colors.black, // Couleur de fond du GridView
+          child: SizedBox(
+            height:
+                MediaQuery.of(context).size.height * 0.6, // Définit une hauteur
+            child: GridView.builder(
+              padding: EdgeInsets.only(
+                left: 10,
+                right: 10,
+                top: 40,
+              ),
+              itemCount: l1.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                mainAxisSpacing: 5,
+                crossAxisSpacing: 10,
+              ),
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: isOperator(l1[index])
+                        ? Colors.amber[900]
+                        : Colors.grey[200],
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Center(
+                    child: Text(
+                      l1[index],
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                );
+              },
             ),
-            itemCount: l1.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-            ),
-            itemBuilder: (context, index) {
-              return Container(
-                child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Center(
-                      child: Text(
-                        l1[index],
-                        style: TextStyle(fontSize: 25),
-                      ),
-                    )),
-              );
-            },
           ),
         ),
       ],
